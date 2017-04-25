c38afcced84d752677b048ea1dfe0d26b4548eeb
Bastien Barthet <bastien.barthet@gmail.com>
2017-04-25 21:28:30 +0200

    add ping service

--- src/main/java/server/GameServer.java
+++ src/main/java/server/GameServer.java
@@ -21,16 +21,30 @@ public class GameServer {
 	private final static Map<String, Game> gameList = new HashMap<>();
 
 	public static void main(String[] args) throws Exception {
-		final int port = Integer.valueOf(System.getenv("$PORT"));
+		final int port = 8000;//Integer.valueOf(System.getenv("$PORT"));
 		final HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
 		server.createContext("/getGame", new GetGameHandler());
 		server.createContext("/createGame", new CreateGameHandler());
 		server.createContext("/updateGame", new UpdateGameHandler());
 		server.createContext("/deleteGame", new DeleteGameHandler());
 		server.createContext("/listGame", new ListGameHandler());
+		server.createContext("/", new PingHandler());
 		server.start();
 	}
 
+	private static class PingHandler implements HttpHandler {
+		public void handle(final HttpExchange t) throws IOException {
+			try (final OutputStream os = t.getResponseBody()) {
+				final byte[] byteArray = "alive".getBytes();
+				System.out.println("Ping Alive.");
+				t.sendResponseHeaders(HttpStatus.SC_OK, byteArray.length);
+				os.write(byteArray);
+			} catch (final IOException e) {
+				t.sendResponseHeaders(HttpStatus.SC_METHOD_FAILURE, 0);
+			}
+		}
+	}
+	
 	private static class GetGameHandler implements HttpHandler {
 		public void handle(final HttpExchange t) throws IOException {
 			try (final OutputStream os = t.getResponseBody()) {
