2ac59adf0933021310aaf3a216eb5b176725bf6f
Bastien Barthet <bastien.barthet@gmail.com>
2017-12-08 19:20:03 +0100

    add controllers and remove useless classes

--- src/main/java/server/RestGameServer.java
+++ src/main/java/server/RestGameServer.java
@@ -1,37 +1,15 @@
 package server;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.boradgames.bastien.schotten_totten.core.exceptions.EmptyDeckException;
-import com.boradgames.bastien.schotten_totten.core.exceptions.GameCreationException;
-import com.boradgames.bastien.schotten_totten.core.exceptions.HandFullException;
-import com.boradgames.bastien.schotten_totten.core.exceptions.MilestoneSideMaxReachedException;
-import com.boradgames.bastien.schotten_totten.core.exceptions.NoPlayerException;
-import com.boradgames.bastien.schotten_totten.core.exceptions.NotYourTurnException;
-import com.boradgames.bastien.schotten_totten.core.model.Card;
-import com.boradgames.bastien.schotten_totten.core.model.Game;
-import com.boradgames.bastien.schotten_totten.core.model.Milestone;
-import com.boradgames.bastien.schotten_totten.core.model.Player;
-import com.boradgames.bastien.schotten_totten.core.model.PlayingPlayerType;
 
 @SpringBootApplication
 public class RestGameServer {
 	
-	private static Map<String, ServerGameManager> gameManagerMap;
 	private static ConfigurableApplicationContext context;
 
 	public static void main(String[] args) {
-		gameManagerMap = new HashMap<>();
 		context = SpringApplication.run(RestGameServer.class, args);
 	}
 	
@@ -42,102 +20,5 @@ public class RestGameServer {
     public static void stop() {
         SpringApplication.exit(context);
     }
-
-	@RestController
-	private class Controlers {
-
-		@RequestMapping("/ping")
-		public String ping() {
-			return new Date().toString() + " - it is time to SCHOTTEN !!!!";
-		}
-		
-		@RequestMapping("/createGame")
-		public boolean createGame(@RequestParam(value="gamename") String gamename) {
-			if (gameManagerMap.containsKey(gamename)) {
-				return false;
-			} else {
-				try {
-					final ServerGameManager gameManager = new ServerGameManager("Player 1", "Player 2", gamename);
-					gameManagerMap.put(gamename, gameManager);
-					return true;
-				} catch (GameCreationException e) {
-					return false;
-				}
-			}
-		}
-		
-		@RequestMapping("/getLastPlayedCard")
-	    public Card getLastPlayedCard(@RequestParam(value="gamename") String gamename) {
-	        return gameManagerMap.get(gamename).getGame().getGameBoard().getLastPlayedCard();
-	    }
-
-	    @RequestMapping("/reclaimMilestone")
-	    public boolean reclaimMilestone(
-	    		@RequestParam(value="gamename") String gamename, 
-	    		@RequestParam(value="p") PlayingPlayerType p, 
-	    		@RequestParam(value="milestoneIndex") int milestoneIndex) throws NotYourTurnException {
-	        final Game game = gameManagerMap.get(gamename).getGame();
-			if (game.getPlayingPlayerType() == p) {
-	            final Milestone milestone = game.getGameBoard().getMilestones().get(milestoneIndex);
-	            return milestone.reclaim(p, game.getGameBoard().getCardsNotYetPlayed());
-	        } else {
-	            throw new NotYourTurnException(p);
-	        }
-	    }
-
-	    @RequestMapping("/getPlayer")
-	    public Player getPlayer(
-	    		@RequestParam(value="gamename") String gamename, 
-	    		@RequestParam(value="p") PlayingPlayerType p) {
-	    	final Game game = gameManagerMap.get(gamename).getGame();
-	        return game.getPlayer(p);
-	    }
-
-	    @RequestMapping("/getPlayingPlayer")
-	    public Player getPlayingPlayer(@RequestParam(value="gamename") String gamename) {
-	    	final Game game = gameManagerMap.get(gamename).getGame();
-	        return game.getPlayingPlayer();
-	    }
-
-	    @RequestMapping("/getWinner")
-	    public Player getWinner(@RequestParam(value="gamename") String gamename) throws NoPlayerException {
-	    	final Game game = gameManagerMap.get(gamename).getGame();
-	        return game.getWinner();
-	    }
-
-	    @RequestMapping("/getMilestones")
-	    public List<Milestone> getMilestones(@RequestParam(value="gamename") String gamename) {
-	    	final Game game = gameManagerMap.get(gamename).getGame();
-	        return game.getGameBoard().getMilestones();
-	    }
-
-	    @RequestMapping("/playerPlays")
-	    public void playerPlays(
-	    		@RequestParam(value="gamename") String gamename, 
-	    		@RequestParam(value="p") PlayingPlayerType p, 
-	    		@RequestParam(value="indexInPlayingPlayerHand") int indexInPlayingPlayerHand, 
-	    		@RequestParam(value="milestoneIndex") int milestoneIndex)
-	            throws NotYourTurnException, EmptyDeckException, HandFullException, MilestoneSideMaxReachedException {
-
-	    	final Game game = gameManagerMap.get(gamename).getGame();
-	        if (game.getPlayingPlayerType() == p) {
-	            final Card cardToPlay = game.getPlayingPlayer().getHand().playCard(indexInPlayingPlayerHand);
-	            try {
-	                game.getGameBoard().getMilestones().get(milestoneIndex).addCard(cardToPlay, p);
-	                game.getGameBoard().updateLastPlayedCard(cardToPlay);
-	                game.getPlayingPlayer().getHand().addCard(
-	                		game.getGameBoard().getDeck().drawCard(), indexInPlayingPlayerHand);
-	                game.swapPlayingPlayerType();
-	            } catch (final MilestoneSideMaxReachedException e) {
-	                game.getPlayingPlayer().getHand().addCard(cardToPlay, indexInPlayingPlayerHand);
-	                throw e;
-	            }
-
-	        } else {
-	            throw new NotYourTurnException(p);
-	        }
-	    }
-		
-	}
 
 }
--- src/test/java/server/QuickClientTest.java
+++ src/test/java/server/QuickClientTest.java
@@ -8,6 +8,8 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import com.boradgames.bastien.schotten_totten.core.exceptions.HandFullException;
@@ -22,14 +24,14 @@ import com.boradgames.bastien.schotten_totten.core.model.PlayingPlayerType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class QuickClientTest {
-	
+
 	@Before
 	public void Before() {
 		final String[] nothing = {};
 		RestGameServer.main(nothing);
 		Assert.assertTrue(RestGameServer.isActive());
 	}
-	
+
 	@After
 	public void After() {
 		RestGameServer.stop();
@@ -50,53 +52,71 @@ public class QuickClientTest {
 		final String gamename = "test-2" + System.currentTimeMillis();
 		final String url = "http://localhost:8080/createGame?gamename=" + gamename;
 		final Boolean result = rest.getForObject(url, Boolean.class);
-//		System.out.println(result);
+		//		System.out.println(result);
 		Assert.assertTrue(result);
 
 		// get milestones
 		final String getMilestones = "http://localhost:8080/getMilestones?gamename=" + gamename;
-		
+
 		final List<Milestone> milestones = Arrays.asList(rest.getForEntity(getMilestones, Milestone[].class).getBody());
 		for (final Milestone m : milestones) {
-//			System.out.println(m.getCaptured().toString());
+			//			System.out.println(m.getCaptured().toString());
 			Assert.assertEquals(MilestonePlayerType.NONE, m.getCaptured());
 		}
 
-//		// get the last played card
-//		final String getlastPlayedCard = "http://localhost:8080/getLastPlayedCard?gamename=" + gamename;
-//		final Card card = rest.getForObject(getlastPlayedCard, Card.class);
-//		System.out.println(card.getColor().toString() + "-" + card.getNumber().toString());
+		//		// get the last played card
+		//		final String getlastPlayedCard = "http://localhost:8080/getLastPlayedCard?gamename=" + gamename;
+		//		final Card card = rest.getForObject(getlastPlayedCard, Card.class);
+		//		System.out.println(card.getColor().toString() + "-" + card.getNumber().toString());
 
 		// get the player
 		final String getPlayingPlayer = "http://localhost:8080/getPlayingPlayer?gamename=" + gamename;
 		final Player player = rest.getForObject(getPlayingPlayer, Player.class);
-//		System.out.println(player.getName() + " - " + player.getPlayerType().toString());
+		//		System.out.println(player.getName() + " - " + player.getPlayerType().toString());
 		Assert.assertEquals(PlayingPlayerType.ONE, player.getPlayerType());
 		Assert.assertEquals("Player 1", player.getName());
+
+		final ResponseEntity<Boolean> resultPlay = rest.getForEntity("http://localhost:8080/playerPlays?"
+				+ "gamename=" + gamename
+				+ "&p=" + PlayingPlayerType.ONE.toString()
+				+ "&indexInPlayingPlayerHand=" + 0
+				+ "&milestoneIndex=" + 0, Boolean.class);
+
+		Assert.assertEquals(HttpStatus.OK, resultPlay.getStatusCode());
+		Assert.assertTrue(resultPlay.getBody().booleanValue());
+		
+//		final ResponseEntity<Boolean> resultPlay2 = rest.getForEntity("http://localhost:8080/playerPlays?"
+//				+ "gamename=" + gamename
+//				+ "&p=" + PlayingPlayerType.TWO.toString()
+//				+ "&indexInPlayingPlayerHand=" + 0
+//				+ "&milestoneIndex=" + 0, Boolean.class);
+//
+//		Assert.assertEquals(HttpStatus.UNAUTHORIZED, resultPlay2.getStatusCode());
+//		Assert.assertFalse(resultPlay2.getBody().booleanValue());
 	}
-	
+
 	@Test
 	public void TestJackson() throws IOException, HandFullException {
 		final ObjectMapper mapper = new ObjectMapper();
 		final String card = mapper.writeValueAsString(new Card(NUMBER.NINE, COLOR.CYAN));
-//		System.out.println(card);
+		//		System.out.println(card);
 		final Card c = mapper.readValue(card, Card.class);
-//		System.out.println(c.getColor().name() + "-" + c.getNumber().name());
+		//		System.out.println(c.getColor().name() + "-" + c.getNumber().name());
 		Assert.assertEquals(NUMBER.NINE, c.getNumber());
 		Assert.assertEquals(COLOR.CYAN, c.getColor());
 
 		final Hand handForTest = new Hand();
 		handForTest.addCard(c, 0);
 		final String hand = mapper.writeValueAsString(handForTest);
-//		System.out.println(hand);
+		//		System.out.println(hand);
 		final Hand h = mapper.readValue(hand, Hand.class);
-//		System.out.println(h.getHandSize());
+		//		System.out.println(h.getHandSize());
 		Assert.assertEquals(1, h.getHandSize());
 		final Card hcard = h.getCards().get(0);
-//		System.out.println(hcard.getColor().name() + "-" + hcard.getNumber().name());
+		//		System.out.println(hcard.getColor().name() + "-" + hcard.getNumber().name());
 		Assert.assertEquals(NUMBER.NINE, hcard.getNumber());
 		Assert.assertEquals(COLOR.CYAN, hcard.getColor());
-		
+
 		final String player = mapper.writeValueAsString(new Player("player1", PlayingPlayerType.ONE));
 		System.out.println(player);
 		final Player p = mapper.readValue(player, Player.class);
@@ -104,5 +124,5 @@ public class QuickClientTest {
 		Assert.assertEquals(PlayingPlayerType.ONE, p.getPlayerType());
 		Assert.assertEquals("player1", p.getName());
 	}
-	
+
 }
